99690c4655aa0ae84833e37e28412f7a4ea25823
SE <58506422+ShukhratE@users.noreply.github.com>
2023-08-07 22:38:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 
 yum install -y docker git
+systemctl enable docker
+systemctl start docker
+
+sudo curl -L https://github.com/docker/compose/releases/download/v2.18.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
