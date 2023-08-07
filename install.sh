685356ea60622405f4223a739d0dc007e4d3d658
SE <58506422+ShukhratE@users.noreply.github.com>
2023-08-07 22:40:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ systemctl start docker
 
 sudo curl -L https://github.com/docker/compose/releases/download/v2.18.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+
+
+mkdir -p /root/logs/{front,rest,temp}
+
+mkdir -p /opt/kaltura/docker
+
+git clone ...
